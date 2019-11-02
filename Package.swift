8d5b4154c3db5e9220e40bb5e225e00c05962096
Ben Gottlieb <ben@standalone.com>
2019-11-01 22:34:03 -0500

    added platform minimum versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "Suite",
+	 platforms: [
+			  .macOS(.v10_15),
+			  .iOS(.v12),
+			  .watchOS(.v5)
+		 ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
