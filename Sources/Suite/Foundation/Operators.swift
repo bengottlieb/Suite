abb99513ddb2b6cec00d7fd85b83c14007b53b4f
Ben Gottlieb <ben@standalone.com>
2019-11-30 19:19:20 -0600

    added Operators.swift

---
+++ Sources/Suite/Foundation/Operators.swift
@@ -0,0 +1,31 @@
+//
+//  Operators.swift
+//  
+//
+//  Created by Ben Gottlieb on 11/30/19.
+//
+
+import Foundation
+
+infix operator ?= : AssignmentPrecedence
+infix operator ∆= : AssignmentPrecedence
+
+public func ?=<T>( left: inout T, right: T?) {
+	guard let value = right else { return }
+	left = value
+}
+
+public func ?=<T>( left: inout T?, right: T?) {
+	guard let value = right else { return }
+	left = value
+}
+
+@discardableResult public func ∆=<T: Equatable>( left: inout T, right: T) -> T {
+	if left != right { left = right }
+	return left
+}
+
+@discardableResult public func ∆=<T: Equatable>( left: inout T?, right: T) -> T? {
+	if left != right { left = right }
+	return left
+}
