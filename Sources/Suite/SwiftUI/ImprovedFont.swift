5bf8af01d01c6ff201a5c4fd838a189ca67cd15a
Ben Gottlieb <ben@standalone.com>
2020-03-31 16:52:34 -0500

    added ImprovedFont

---
+++ Sources/Suite/SwiftUI/ImprovedFont.swift
@@ -0,0 +1,27 @@
+//
+//  ImprovedFont.swift
+//  
+//
+//  Created by Ben Gottlieb on 3/31/20.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available(OSX 10.15, iOS 13.0, *)
+public struct ImprovedFont {
+	public let family: String
+	public let size: CGFloat
+	
+	public init(_ family: String, size: CGFloat) {
+		self.family = family
+		self.size = size
+	}
+	
+	public var font: Font { Font.custom(family, size: size) }
+	
+	public func ofSize(_ size: CGFloat) -> Font { Font.custom(family, size: size) }
+	
+	public func bumpSize(_ delta: CGFloat) -> Font { Font.custom(family, size: size + delta) }
+}
+#endif
