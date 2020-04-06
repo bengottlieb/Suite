bb109400de6d2c6cd2328bd23e0a27423f2a5907
Ben Gottlieb <ben@standalone.com>
2020-04-06 05:58:56 -0500

    added InterfaceOrientedView

--- Sources/Suite/SwiftUI/InterfaceOrientedView.swift
+++ Sources/Suite/SwiftUI/InterfaceOrientedView.swift
@@ -8,13 +8,12 @@
 #if canImport(Combine)
 import SwiftUI
 import Combine
-import Suite
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
-class OrientationWatcher: ObservableObject, CustomStringConvertible {
-	static var instance = OrientationWatcher()
+public class OrientationWatcher: ObservableObject, CustomStringConvertible {
+	public static var instance = OrientationWatcher()
 	
-	static func setup(windowScene: UIWindowScene) {
+	public static func setup(windowScene: UIWindowScene) {
 		self.instance = OrientationWatcher(initialOrientation: windowScene.interfaceOrientation)
 	}
 	
@@ -28,20 +27,23 @@ class OrientationWatcher: ObservableObject, CustomStringConvertible {
 			}.sequester().unsequester()
 	}
 	
-	@Published var orientation: UIInterfaceOrientation
+	@Published public var orientation: UIInterfaceOrientation
 	private var subscription: AnyCancellable!
 	
-	var isLandscape: Bool { return self.orientation.isLandscape }
-	var description: String { return self.isLandscape ? "Landscape" : "Portrait" }
+	public var isLandscape: Bool { return self.orientation.isLandscape }
+	public var description: String { return self.isLandscape ? "Landscape" : "Portrait" }
 }
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
-struct InterfaceOrientedView<Contents: View>: View {
+public struct InterfaceOrientedView<Contents: View>: View {
 	@ObservedObject var orientationWatcher = OrientationWatcher.instance
 	
 	let contents: () -> Contents
+	public init(contents: @escaping () -> Contents) {
+		self.contents = contents
+	}
 	
-    var body: some View {
+    public var body: some View {
 		contents()
 			.id(orientationWatcher.isLandscape)
 	}
