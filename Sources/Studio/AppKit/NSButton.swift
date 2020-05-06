c3631107e0b1059ae5fd124ad8a11d2cc5ab6f06
Ben Gottlieb <ben@standalone.com>
2020-05-06 04:55:18 -0500

    added NSButton.visiblyEnabled and .bezel

--- Sources/Studio/AppKit/NSButton.swift
+++ Sources/Studio/AppKit/NSButton.swift
@@ -9,10 +9,22 @@
 
 import AppKit
 
-extension NSButton {
-	public var isOn: Bool {
+public extension NSButton {
+	var isOn: Bool {
 		get { return self.state == NSControl.StateValue.on }
 		set { self.state = newValue ? NSControl.StateValue.on : NSControl.StateValue.off }
+	}
+	
+	@discardableResult func visiblyEnabled(_ enabled: Bool, disabledAlpha alpha: Float = 0.6) -> Self {
+		self.wantsLayer = true
+		self.layer?.opacity = enabled ? 1.0 : alpha
+		self.isEnabled = enabled
+		return self
+	}
+	
+	@discardableResult func bezel(_ bezel: NSButton.BezelStyle) -> Self {
+		self.bezelStyle = bezel
+		return self
 	}
 }
 
