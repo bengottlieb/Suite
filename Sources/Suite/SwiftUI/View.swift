1dfb23b841f30f2f020e93da721c699e693411d7
Ben Gottlieb <ben@standalone.com>
2020-05-14 06:11:06 -0500

    added SFSymbols.swift

--- Sources/Suite/SwiftUI/View.swift
+++ Sources/Suite/SwiftUI/View.swift
@@ -12,9 +12,13 @@ import SwiftUI
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
 public extension View {
-    func frame(size: CGSize) -> some View {
-        frame(width: size.width, height: size.height)
-    }
+	func frame(size: CGSize) -> some View {
+		frame(width: size.width, height: size.height)
+	}
+	
+	func frame(_ size: CGFloat) -> some View {
+		frame(width: size, height: size)
+	}
 }
 #endif
 #endif
