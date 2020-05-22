0d12713adf299e14c9f782f4c4898b71af20e19c
Ben Gottlieb <ben@standalone.com>
2020-05-22 15:47:31 -0500

    cleaned up Bindings extension

--- Sources/Suite/Utilities/Binding.swift
+++ Sources/Suite/Utilities/Binding.swift
@@ -18,3 +18,36 @@ public extension Binding {
 		return Binding<Value?>(get: { return value }) { _ in }
 	}
 }
+
+@available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
+public extension Binding where Value: Equatable {
+	init(_ source: Binding<Value?>, nilValue: Value) {
+		self.init(
+			get: { source.wrappedValue ?? nilValue },
+			set: { newValue in
+				if newValue == nilValue {
+					source.wrappedValue = nil
+				} else {
+					source.wrappedValue = newValue
+				}
+			})
+	}
+}
+
+@available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
+public extension Binding {
+	var optional: Binding<Value?> {
+		Binding<Value?>(get: { self.wrappedValue }, set: { opt in
+			if let val = opt { self.wrappedValue = val }
+		})
+	}
+	
+	init(readonly: Value) {
+		self.init(get: { return readonly }, set: { _ in })
+	}
+	
+	init<T>(isNotNil source: Binding<T?>, defaultValue: T) where Value == Bool {
+		self.init(get: { source.wrappedValue != nil }, set: { source.wrappedValue = $0 ? defaultValue : nil })
+	}
+}
+
