4f737919b2cdd02527c16979899aec1bdeda6a28
Ben Gottlieb <ben@standalone.com>
2020-05-23 15:49:44 -0500

    cleaned up .midnight, added .midnightGMT

--- Sources/Studio/Foundation/DateFormatter.swift
+++ Sources/Studio/Foundation/DateFormatter.swift
@@ -8,8 +8,18 @@
 import Foundation
 
 public extension DateFormatter {
-	static let iso8601 = DateFormatter(format: "yyyy-MM-dd'T'HH:mm:ss'Z'")
+//	static let iso8601 = DateFormatter(format: "yyyy-MM-dd'T'HH:mm:ss'Z'")
 	
+	static let iso8601: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.calendar = Calendar(identifier: .iso8601)
+		formatter.locale = Locale(identifier: "en_US_POSIX")
+		formatter.timeZone = TimeZone(abbreviation: "UTC")
+		
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+		return formatter
+	}()
+
 	convenience init(format: String) {
 		self.init()
 		self.dateFormat = format
