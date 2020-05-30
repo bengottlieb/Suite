92b15805f32a0820a79e61e4145f0e1184c9f3c8
Ben Gottlieb <ben@standalone.com>
2020-05-30 17:34:22 -0500

    added Result.isSuccess

---
+++ Sources/Studio/Foundation/Result.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Gottlieb on 5/30/20.
+//
+
+import Foundation
+
+extension Result {
+	public var isSuccess: Bool {
+		switch self {
+		case .failure(_): return false
+		case .success(_): return true
+		}
+	}
+}
