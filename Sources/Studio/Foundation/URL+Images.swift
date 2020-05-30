21ff8baacdede9436b0b502b2247b91a55d28aa6
Ben Gottlieb <ben@standalone.com>
2020-05-30 06:35:50 -0500

    added method to resize an image on-disk

---
+++ Sources/Studio/Foundation/URL+Images.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by ben on 5/30/20.
+//
+
+import Foundation
+import CoreGraphics
+
+extension URL {
+	private func resizedImage(maxDimension: CGFloat) -> CGImage? {
+		let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+		let downsampleOptions =  [kCGImageSourceCreateThumbnailFromImageAlways: true,
+								  kCGImageSourceShouldCacheImmediately: true,
+								  kCGImageSourceCreateThumbnailWithTransform: true,
+								  kCGImageSourceThumbnailMaxPixelSize: maxDimension] as CFDictionary
+
+		guard
+			let imageSource = CGImageSourceCreateWithURL(self as CFURL, imageSourceOptions),
+			let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)
+		else {
+			return nil
+		}
+
+		return downsampledImage
+	}
+}
+
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
+import AppKit
+extension URL {
+	/// Resize an image on-disk without first loading it in
+	func resizedContainedImage(maxDimension: CGFloat) -> NSImage? {
+		guard let cgImage = self.resizedImage(maxDimension: maxDimension) else { return nil }
+		return NSImage(cgImage: cgImage, size: NSSize(width: maxDimension, height: maxDimension))
+	}
+}
+#endif
+
+#if canImport(UIKit)
+import UIKit
+extension URL {
+	/// Resize an image on-disk without first loading it in
+	func resizedContainedImage(maxDimension: CGFloat) -> UIImage? {
+		guard let cgImage = self.resizedImage(maxDimension: maxDimension) else { return nil }
+		return UIImage(cgImage: cgImage)
+	}
+}
+#endif
+
+
+
+
