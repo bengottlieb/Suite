e16288112d3608fd6f0faedab8cb927ada1425d4
Ben Gottlieb <ben@standalone.com>
2020-06-22 15:37:11 -0500

    added Gestalt.IPAddress

--- Sources/Studio/Types/Gestalt.swift
+++ Sources/Studio/Types/Gestalt.swift
@@ -233,4 +233,34 @@ public struct Gestalt {
 	public static var isRunningUnitTests: Bool = {
 		return NSClassFromString("XCTest") != nil
 	}()
+	
+	public static var IPAddress: String? {
+		 var address: String?
+		 var ifaddr: UnsafeMutablePointer<ifaddrs>?
+		 if getifaddrs(&ifaddr) == 0 {
+			  var ptr = ifaddr
+			  while ptr != nil {
+					defer { ptr = ptr?.pointee.ifa_next }
+					let interface = ptr?.pointee
+					let addrFamily = interface?.ifa_addr.pointee.sa_family
+					if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6),
+						 let cString = interface?.ifa_name,
+						 String(cString: cString) == "en0",
+						 let saLen = (interface?.ifa_addr.pointee.sa_len) {
+						 var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+						 let ifaAddr = interface?.ifa_addr
+						 getnameinfo(ifaAddr,
+										 socklen_t(saLen),
+										 &hostname,
+										 socklen_t(hostname.count),
+										 nil,
+										 socklen_t(0),
+										 NI_NUMERICHOST)
+						 address = String(cString: hostname)
+					}
+			  }
+			  freeifaddrs(ifaddr)
+		 }
+		 return address
+	}
 }
