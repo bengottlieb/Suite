8d35a2336dea9a69aad72c6e0cf7f5d9abae7755
Ben Gottlieb <ben@standalone.com>
2020-09-18 20:59:21 -0500

    added SeededRandomNumberGenerator

---
+++ Sources/Studio/Types/SeededRandomNumberGenerator.swift
@@ -0,0 +1,24 @@
+//
+//  SeededRandomNumberGenerator.swift
+//  
+//
+//  Created by Ben Gottlieb on 9/18/20.
+//
+
+import Foundation
+import GameKit
+
+public struct SeededRandomNumberGenerator: RandomNumberGenerator {
+	private let mersenne: GKMersenneTwisterRandomSource
+
+	public mutating func next() -> UInt64 {
+		let next1 = UInt64(bitPattern: Int64(mersenne.nextInt()))
+		let next2 = UInt64(bitPattern: Int64(mersenne.nextInt()))
+		return next1 ^ (next2 << 32)
+	}
+	
+	public init(seed: Int = Int(Date().timeIntervalSinceReferenceDate)) {
+		self.mersenne = GKMersenneTwisterRandomSource(seed: UInt64(seed))
+	}
+	
+}
