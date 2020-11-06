69e00e117620e66ac348d6d811e532542171c39d
Ben Gottlieb <ben@standalone.com>
2020-11-06 05:21:19 -0600

    added StringIdentifiable

--- Sources/Studio/Foundation/StringIdentifiable.swift
+++ Sources/Studio/Foundation/StringIdentifiable.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+#if canImport(Combine)
+	
+	public protocol StringIdentifiable: Identifiable where ID: StringProtocol {
+	}
+
+#else
+
+	public protocol StringIdentifiable: Identifiable {
+		var id: String { get }
+	}
+
+#endif
