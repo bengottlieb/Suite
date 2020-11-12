834630e3e8f3c0b4c7bcc87871a03ea4ea0b0d94
Ben Gottlieb <ben@standalone.com>
2020-11-11 21:52:49 -0600

    improved sequester/unsequestering

--- Sources/Suite/SwiftUI/Subscription.swift
+++ Sources/Suite/SwiftUI/Subscription.swift
@@ -18,17 +18,22 @@ public var SubscriptionDictionary: [String: AnyCancellable] = [:]
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
 public extension AnyCancellable {
-	static func unsequester(_ key: String) {
-		SubscriptionDictionary.removeValue(forKey: key)
+	static func unsequester(_ key: String? = nil, file: String = #file, function: String = #function) {
+		SubscriptionDictionary.removeValue(forKey: key ?? "\(file)#\(function)")
 	}
 	
 	@discardableResult
-	func sequester(key: String? = nil) -> Self {
-		if let key = key {
-			SubscriptionDictionary[key] = self
-		} else {
-			self.store(in: &SubscriptionBag)
-		}
+	func sequester(file: String = #file, function: String = #function) -> Self {
+		let key = "\(file)#\(function)"
+		assert(SubscriptionDictionary[key] == nil, "Already sequestered a subscription in \(file): \(function).")
+		SubscriptionDictionary[key] = self
+		return self
+	}
+
+	@discardableResult
+	func sequester(_ key: String) -> Self {
+		assert(SubscriptionDictionary[key] == nil, "Already sequestered a subscription using \(key).")
+		SubscriptionDictionary[key] = self
 		return self
 	}
 
