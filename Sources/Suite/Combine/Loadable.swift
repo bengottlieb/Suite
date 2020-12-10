17eb854d0050a325ba7d72f77c2be52ad76eec93
Ben Gottlieb <ben@standalone.com>
2020-12-09 18:28:15 -0600

    fixed a combine issue

--- Sources/Suite/Combine/Loadable.swift
+++ Sources/Suite/Combine/Loadable.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+
+#if canImport(Combine)
 import Combine
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
@@ -14,3 +16,4 @@ public protocol Loadable: ObservableObject {
 	var state: LoadingState<Output> { get }
 	func load()
 }
+#endif
