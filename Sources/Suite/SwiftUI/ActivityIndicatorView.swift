d09ed7336a04be979a622e1e6055f16689ed9dfc
Ben Gottlieb <ben@standalone.com>
2020-12-17 21:15:46 -0600

    first pass at pull-to-refresh in SwiftUI

--- Sources/Suite/SwiftUI/ActivityIndicatorView.swift
+++ Sources/Suite/SwiftUI/ActivityIndicatorView.swift
@@ -12,35 +12,36 @@ import SwiftUI
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
 public struct ActivityIndicatorView: View {
 	private var color: Color
-	private var spokeCount = 12
+	private var spokeCount = 8
 	private var period: TimeInterval = 1.2
+	private var fixedPercent: Double?
+	
 	@State private var gradientRotation = Angle.zero
 	
-	public init(_ color: Color = .gray) {
+	public init(_ color: Color = .gray, fixedPercent: Double? = nil) {
 		self.color = color
+		self.fixedPercent = fixedPercent
 	}
 	
-	public func small() -> some View {
-		self.frame(width: 20, height: 20)
-	}
-	
-	public func large() -> some View {
-		self.frame(width: 50, height: 50)
-	}
+	public func small() -> some View { self.frame(width: 20, height: 20) }
+	public func medium() -> some View { self.frame(width: 35, height: 35) }
+	public func large() -> some View { self.frame(width: 50, height: 50) }
 	
 	public var body: some View {
 		AngularGradient(gradient: .init(colors: [.clear, self.color]), center: .center)
 			.rotationEffect(self.gradientRotation)
 			.animation(Animation.linear(duration: period).repeatForever(autoreverses: false))
 			.mask(self.spokes)
-			.onAppear { self.gradientRotation = .radians(2 * .pi) }
+			.onAppear {
+				if fixedPercent == nil { self.gradientRotation = .radians(2 * .pi) }
+			}
 	}
 	
 	var spokes: some View {
 		GeometryReader { geo in
-			ForEach(0..<self.spokeCount) { index in
+			ForEach(0..<Int(Double(self.spokeCount) * (fixedPercent ?? 1)), id: \.self) { index in
 				RoundedRectangle(cornerRadius: 2)
-					.frame(width: geo.size.width / 20, height: geo.size.height / 5)
+					.frame(width: geo.size.width / 10, height: geo.size.height / 4)
 					.position(x: geo.size.width / 2, y: geo.size.height / 5)
 					.rotationEffect(.radians(2 * .pi * Double(index) / Double(self.spokeCount)))
 			}
---
+++ Sources/Suite/SwiftUI/PullToRefreshView.swift
@@ -0,0 +1,70 @@
+//
+//  PullToRefreshIndicator.swift
+//  
+//
+//  Created by Ben Gottlieb on 12/17/20.
+//
+
+#if canImport(Combine)
+#if canImport(UIKit)
+
+import SwiftUI
+import UIKit
+
+#if os(iOS)
+@available(iOS 13.0, *)
+public struct PullToRefreshIndicator: View {
+	let refresh: (@escaping () -> Void) -> Void
+	let spinnerColor: Color
+	public init(color: Color = .gray, refresh callback: @escaping (@escaping () -> Void) -> Void) {
+		spinnerColor = color
+		refresh = callback
+	}
+	
+	func stopRefreshing() {
+		self.percentFull = 0
+		withAnimation() {
+			self.isRefreshing = false
+		}
+	}
+	
+	static var safeAreaTop = (UIApplication.shared.currentWindow?.rootViewController?.view.safeAreaInsets.top ?? 20)
+	var fullHeight: CGFloat = 50 + Self.safeAreaTop
+	@State var percentFull = 0.0
+	@State var isRefreshing = false
+	
+	public var body: some View {
+		ZStack(alignment: .bottom) {
+			GeometryReader() { proxy -> Color in
+				let height = proxy.frame(in: .global).y / fullHeight
+				DispatchQueue.main.async {
+					percentFull = max(0, min(Double(height), 1))
+					withAnimation() {
+						if percentFull >= 1.0, !isRefreshing {
+							isRefreshing = true
+							refresh() { stopRefreshing() }
+						}
+					}
+				}
+				
+				return Color.clear
+			}
+			.frame(height: isRefreshing ? fullHeight : 0)
+			.layoutPriority(1)
+			
+			Group() {
+				if isRefreshing {
+					ActivityIndicatorView(spinnerColor).medium()
+				} else {
+					ActivityIndicatorView(spinnerColor, fixedPercent: percentFull).medium()
+				}
+			}
+			.padding(.bottom, 10)
+
+		}
+	}
+}
+
+#endif
+#endif
+#endif
