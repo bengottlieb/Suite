0e452455f9d06638643f67ebf255a60143a71ebb
Ben Gottlieb <ben@standalone.com>
2021-01-07 07:53:55 -0600

    added URLImage

---
+++ Sources/Suite/SwiftUI/URLImage.swift
@@ -0,0 +1,57 @@
+//
+//  URLImage.swift
+//  
+//
+//  Created by Ben Gottlieb on 1/7/21.
+//
+
+import Foundation
+
+#if canImport(SwiftUI)
+#if canImport(Combine)
+
+import SwiftUI
+import Combine
+
+@available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
+public struct URLImage: View {
+    let placeholder: Image?
+    let imageURL: URL?
+    let contentMode: ContentMode
+    @State var uiImage: UIImage?
+    
+    public init(url: URL?, contentMode: ContentMode = .fit, placeholder: Image? = nil) {
+        imageURL = url
+        self.contentMode = contentMode
+        self.placeholder = placeholder
+    }
+    
+    var imageView: Image {
+        if let image = uiImage {
+            return Image(uiImage: image)
+        }
+        
+        if let placeholder = placeholder { return placeholder }
+        return Image(.square)
+    }
+    
+    public var body: some View {
+        imageView
+            .resizable()
+            .aspectRatio(contentMode: contentMode)
+            .onAppear() {
+                if let imageURL = imageURL {
+                    ImageCache.instance.fetch(for: imageURL)
+                        .onSuccess { image in
+                            uiImage = image
+                        }
+                }
+            }
+    }
+}
+
+
+
+
+#endif
+#endif
