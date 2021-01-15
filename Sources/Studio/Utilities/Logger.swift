426e3ac7c910020cd82a415c0147db2fdb65492b
Ben Gottlieb <ben@standalone.com>
2021-01-14 21:44:15 -0600

    added logToFile

--- Sources/Studio/Utilities/Logger.swift
+++ Sources/Studio/Utilities/Logger.swift
@@ -36,13 +36,58 @@ public class Logger {
 	
 	private init() { }
 	
+	public var fileURL: URL?
+	var logFileExists = false
 	public var showTimestamps = true { didSet { self.timestampStart = Date() }}
 	public var timestampStart = Date()
+	
+	public func log(to url: URL, clearingFirst: Bool = true) {
+		fileURL = url
+		
+		try? FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
+		if clearingFirst {
+			try? FileManager.default.removeItem(at: url)
+		} else {
+			logFileExists = FileManager.default.fileExists(at: url)
+		}
+	}
 	
 	public enum Special { case `break` }
 	public enum Level: Int, Comparable {
 		case off, quiet, mild, loud, verbose
 		public static func <(lhs: Level, rhs: Level) -> Bool { return lhs.rawValue < rhs.rawValue }
+	}
+	
+	func output(_ string: String) {
+		print(string)
+		
+		if let url = fileURL, let data = string.data(using: .utf8) {
+			write(data, to: url)
+		}
+	}
+	
+	func write(_ data: Data, to url: URL) {
+		do {
+			if logFileExists {
+				let linefeed = "\n".data(using: .utf8)!
+				
+				let file = try FileHandle(forUpdating: url)
+				if #available(iOS 13.4, *) {
+					try file.seekToEnd()
+				} else {
+					file.seekToEndOfFile()
+				}
+				file.write(data)
+				file.write(linefeed)
+				file.closeFile()
+			} else {
+				try data.write(to: url)
+				logFileExists = true
+				write("".data(using: .utf8)!, to: url)
+			}
+		} catch {
+			print("Failed to log to file: \(error)")
+		}
 	}
 	
 	public var level: Level = {
@@ -54,7 +99,7 @@ public class Logger {
 	public func log(_ special: Special, level: Logger.Level = .mild) {
 		if level > self.level { return }
 		switch special {
-		case .break: print("\n")
+		case .break: output("\n")
 		}
 	}
 	
@@ -63,13 +108,13 @@ public class Logger {
 		var message = msg()
 		
 		if showTimestamps { message = String(format: "%.04f - ", Date().timeIntervalSince(timestampStart)) + message }
-		print(message)
+		output(message)
 	}
 	
 	public func log(error: Error?, _ msg: @autoclosure () -> String, level: Level = .mild) {
 		if level > self.level { return }
 		let message = "⚠️ \(msg()) \(error?.localizedDescription ?? "")"
-		print(message)
+		output(message)
 	}
 }
 
