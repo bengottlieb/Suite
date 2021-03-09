798cf7b43dc33d7daa7867a3041b9c658f659675
Ben Gottlieb <ben@standalone.com>
2021-03-08 21:17:34 -0600

    added some time-related functions

--- Sources/Studio/Foundation/Date.Time.swift
+++ Sources/Studio/Foundation/Date.Time.swift
@@ -28,6 +28,33 @@ public extension Date {
 			lhs.timeInterval == rhs.timeInterval
 		}
 		
+		public func byAdding(hours: Int = 0, minutes: Int = 0, seconds: TimeInterval = 0) -> Time {
+			var second = self.second + seconds
+			var minute = self.minute + minutes
+			var hour = (self.hour + hours + 24) % 24
+			
+			if second < 0 {
+				minute -= 1
+				second += 60
+			} else if second > 60 {
+				minute += 1
+				second -= 60
+			}
+
+			if minute < 0 {
+				hour -= 1
+				minute += 60
+			} else if minute > 60 {
+				hour += 1
+				minute -= 60
+			}
+			return Time(hour: hour, minute: minute, second: second)
+		}
+		
+		public static func -(lhs: Time, rhs: Time) -> Time {
+			lhs.byAdding(hours: -rhs.hour, minutes: -rhs.minute, seconds: -rhs.second)
+		}
+		
 		public init(hour: Int, minute: Int, second: TimeInterval = 0) {
 			self.hour = min(hour, 23)
 			self.minute = min(minute, 59)
@@ -51,6 +78,12 @@ public extension Date {
 			}
 			
 			return Time.lastSecond.timeInterval(since: other) + self.timeInterval(since: .midnight)
+		}
+		
+		init(timeInterval: TimeInterval) {
+			hour = Int(timeInterval / 3600) % 24
+			minute = Int(timeInterval / 60) % 60
+			second = TimeInterval(Int(timeInterval) % 60)
 		}
 		
 		public var nextHour: Time {
@@ -92,5 +125,14 @@ public extension Date {
 	var time: Time {
 		let components = Calendar.current.dateComponents([.hour, .minute, .second, .nanosecond], from: self)
 		return Time(hour: components.hour ?? 0, minute: components.minute ?? 0, second: TimeInterval(components.second ?? 0) + min(1, TimeInterval(components.nanosecond ?? 0) / 1_000_000_000))
+	}
+}
+
+extension Array where Element == Date.Time {
+	public var average: Date.Time? {
+		guard !isEmpty else { return nil }
+		let sum = self.map { $0.timeInterval }.sum()
+		
+		return Date.Time(timeInterval: sum / TimeInterval(count))
 	}
 }
