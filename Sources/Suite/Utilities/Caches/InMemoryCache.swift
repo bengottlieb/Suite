61a56664f7db6ef43dc2c7e1f3eea2b9afd57501
Ben Gottlieb <ben@standalone.com>
2021-03-14 05:58:42 -0500

    serialize in-memory cache accesses

--- Sources/Suite/Utilities/Caches/InMemoryCache.swift
+++ Sources/Suite/Utilities/Caches/InMemoryCache.swift
@@ -16,6 +16,11 @@ import Combine
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
 public class InMemoryCache<Element: Cachable>: Cache<Element> {
+    let queue = DispatchQueue(label: "_imMemoryCache_\(Element.self)", qos: .userInitiated, attributes: [], autoreleaseFrequency: .inherit, target: nil)
+    
+    @discardableResult
+    private func serialize(_ block: () -> Element?) -> Element? { queue.sync { block() } }
+    
 	struct CachedItem {
 		let cachedAt: Date
 		let item: Element
@@ -40,7 +45,8 @@ public class InMemoryCache<Element: Cachable>: Cache<Element> {
 	public override func fetch(for url: URL, caching: CachePolicy = .normal) -> AnyPublisher<Element, Error> {
 		let cacheKey = key(for: url)
 		
-		if let current = cache[cacheKey], !caching.shouldIgnoreLocal(forDate: current.cachedAt) { return self.just(current.item) }
+        if let result = serialize({ if let current = cache[cacheKey], !caching.shouldIgnoreLocal(forDate: current.cachedAt) { return current.item }; return nil }) { return self.just(result) }
+        
 		return super.fetch(for: url, caching: caching)
 			.flatMap { result -> AnyPublisher<Element, Error> in
 				self.store(result, for: url)
@@ -55,25 +61,35 @@ public class InMemoryCache<Element: Cachable>: Cache<Element> {
 	}
 
 	public override func store(_ element: Element, for url: URL) {
-		let cacheKey = key(for: url)
-		cache[cacheKey] = CachedItem(cachedAt: Date(), item: element)
-		super.store(element, for: url)
+        serialize {
+            storeInCache(element, for: url)
+            return nil
+        }
 	}
+    
+    private func storeInCache(_ element: Element, for url: URL) {
+        let cacheKey = key(for: url)
+        cache[cacheKey] = CachedItem(cachedAt: Date(), item: element)
+        super.store(element, for: url)
+    }
 	
 	public override func cachedValue(for url: URL) -> Element? {
-		if let item = cache[key(for: url)]?.item { return item }
-		
-		if let cached = super.cachedValue(for: url) {
-			store(cached, for: url)
-			return cached
-		}
-		return nil
+        let key = self.key(for: url)
+        return serialize {
+            if let item = self.cache[key]?.item { return item }
+            
+            if let cached = super.cachedValue(for: url) {
+                storeInCache(cached, for: url)
+                return cached
+            }
+            return nil
+        }
 	}
 
 	public override func hasCachedValue(for url: URL) -> Bool {
 		let key = self.key(for: url)
 		
-		if cache[key] != nil { return true }
+        if serialize({ self.cache[key]?.item }) != nil { return true }
 		return super.hasCachedValue(for: url)
 	}
 
