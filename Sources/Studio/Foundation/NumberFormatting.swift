08c587bcb1c4a4eaf7faa83aea0bc4814b51d288
Ben Gottlieb <ben@standalone.com>
2021-03-27 08:22:21 -0500

    improved number formatting

--- Sources/Studio/Foundation/NumberFormatting.swift
+++ Sources/Studio/Foundation/NumberFormatting.swift
@@ -29,14 +29,42 @@ extension Float: DecimalFormattable {
 	public var doubleValue: Double { Double(self) }
 }
 
+private let formatter: NumberFormatter = {
+	let formatter = NumberFormatter()
+	formatter.numberStyle = .decimal
+	formatter.maximumFractionDigits = 2
+	return formatter
+}()
+
 public extension DecimalFormattable {
-	func string(decimalPlaces: Int = 2) -> String {
-		var result = String(format: "%.\(decimalPlaces)f", doubleValue)
+	func string(decimalPlaces: Int = 2, padded: Bool = false) -> String {
+		var result: String!
+			
+		if decimalPlaces <= 2 {
+			result = formatter.string(from: NSNumber(value: doubleValue))
+		}
+		if result == nil { result = String(format: "%.\(decimalPlaces)f", doubleValue) }
 		
-		while result.hasSuffix(".") || result.hasSuffix("0") {
+		while result.hasSuffix("0") || result.decimalPlaces > decimalPlaces {
 			result = String(result.dropLast())
 		}
 		
+		if padded {
+			while result.decimalPlaces < decimalPlaces {
+				result += "0"
+			}
+		} else {
+			if result.hasSuffix(".") { result = String(result.dropLast()) }
+		}
+		
 		return result
+	}
+}
+
+extension String {
+	var decimalPlaces: Int {
+		if !contains(".") { return 0 }
+		let components = self.components(separatedBy: ".")
+		return components.last?.count ?? 0
 	}
 }
