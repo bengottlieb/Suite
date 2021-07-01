d84033655a4e0790f133e91eb6295b16097a5578
Ben Gottlieb <ben@standalone.com>
2021-07-01 05:43:06 -0500

    cleaned up BottomSheet presentation

--- Sources/Suite/SwiftUI/BottomSheetView.swift
+++ Sources/Suite/SwiftUI/BottomSheetView.swift
@@ -42,33 +42,54 @@ public  struct OverlayModifer<Overlay: View, Item>: ViewModifier {
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
 public extension View {
-	func presentDimmedOverlay<Content: View, Item>(item: Binding<Item?>, @ViewBuilder overlayBuilder: @escaping (Item) -> Content) -> some View {
-		blur(radius: item.wrappedValue == nil ? 0 : 3)
-	//		.animation(.linear)
-			.allowsHitTesting(item.wrappedValue == nil)
-			.onTapGesture {
-				print("Hhello")
-			}
+	func presentDimmedOverlay<Content: View, Item>(item: Binding<Item?>, tapToDismiss: Bool = true, @ViewBuilder overlayBuilder: @escaping (Item) -> Content) -> some View {
+		self
+			//.blur(radius: item.wrappedValue == nil ? 0 : 3)
+			.overlay(
+				ZStack() {
+					if item.wrappedValue != nil {
+						Rectangle().fill(Color.black.opacity(0.5))
+							.onTapGesture {
+								if tapToDismiss { withAnimation() { item.wrappedValue = nil } }
+							}
+							.transition(.opacity)
+					}
+				}
+				.animation(.linear)
+			)
 			.modifier(OverlayModifer(item: item, overlay: overlayBuilder))
 	}
 	
-	func presentBottomSheet<Content: View, Item>(item: Binding<Item?>, @ViewBuilder content: @escaping (Item) -> Content) -> some View {
+	func presentBottomSheet<Content: View, Item>(item: Binding<Item?>, background: Color = .black, tapToDismiss: Bool = true, @ViewBuilder content: @escaping (Item) -> Content) -> some View {
+		presentDimmedOverlay(item: item, tapToDismiss: tapToDismiss) { item in
+			BottomSheet(background) { content(item) }
+		}
+	}
+
+	func presentBottomSheet<Content: View, Item, Background: View>(item: Binding<Item?>, background: Background, @ViewBuilder content: @escaping (Item) -> Content) -> some View {
 		presentDimmedOverlay(item: item) { item in
-			BottomSheet() { content(item) }
+			BottomSheet(background) { content(item) }
 		}
 	}
 }
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
-public struct BottomSheet<Content: View>: View {
+public struct BottomSheet<Content: View, Background: View>: View {
+	var backgroundView: Background
 	@ViewBuilder var content: () -> Content
 	
+	public init(_ background: Background, _ content: @escaping () -> Content) {
+		self.backgroundView = background
+		self.content = content
+	}
+
 	public var body: some View {
 		GeometryReader() { geo in
 			VStack() {
 				Spacer()
 				content()
 					.padding(.bottom, geo.safeAreaInsets.bottom)
+					.background(backgroundView)
 					.cornerRadius(16)
 			}
 			.edgesIgnoringSafeArea(.bottom)
@@ -78,5 +99,12 @@ public struct BottomSheet<Content: View>: View {
 	}
 }
 
+@available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 6, *)
+extension BottomSheet where Background == Color {
+	public init(_ background: Color = .black, _ content: @escaping () -> Content) {
+		self.backgroundView = background
+		self.content = content
+	}
+}
 
 #endif
