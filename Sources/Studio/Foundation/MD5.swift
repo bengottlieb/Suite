4ab74a17d46af77cf9e9c6a4b36fc307012faf3e
Ben Gottlieb <ben@standalone.com>
2021-09-10 21:02:52 -0500

    added MD5

---
+++ Sources/Studio/Foundation/MD5.swift
@@ -0,0 +1,33 @@
+//
+//  MD5.swift
+//  
+//
+//  Created by Ben Gottlieb on 9/10/21.
+//
+
+import Foundation
+import CryptoKit
+
+@available(watchOS 6.0, iOS 13.0, *)
+public extension String {
+	var md5: String? {
+		guard let data = data(using: .utf8) else { return nil }
+		return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+	 }
+}
+
+@available(watchOS 6.0, iOS 13.0, *)
+public extension Data {
+	var md5: String? {
+		return Insecure.MD5.hash(data: self).map { String(format: "%02hhx", $0) }.joined()
+	 }
+}
+
+@available(watchOS 6.0, iOS 13.0, *)
+public extension URL {
+	var md5: String? {
+		guard isFileURL, let data = try? Data(contentsOf: self) else { return nil }
+		return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+	 }
+}
+
