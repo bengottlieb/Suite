1cd5298edb16e35788ff12c3124d9b4f1141f194
Ben Gottlieb <ben@standalone.com>
2021-10-09 19:13:42 -0500

    added CaseIterable.next()

--- Sources/Studio/Foundation/Enums.swift
+++ Sources/Studio/Foundation/Enums.swift
@@ -15,4 +15,15 @@ public extension CaseIterable where Self: Equatable {
     var isLastCase: Bool {
         self == Array(Self.allCases).last
     }
+
+	func next() -> Self {
+		let all = Array(Self.allCases)
+		
+		for i in 0..<all.count {
+			if all[i] == self, i < (all.count - 1) {
+				return all[i + 1]
+			}
+		}
+		return all[0]
+	}
 }
