17b72a469d241e247bd1d463f8b854534399fc90
Ben Gottlieb <ben@standalone.com>
2021-11-23 06:11:22 -0600

    added codable and updating support to DefaultsValue

--- Sources/Suite/Property Wrappers/DefaultsValue.swift
+++ Sources/Suite/Property Wrappers/DefaultsValue.swift
@@ -8,26 +8,51 @@
 import Foundation
 
 @propertyWrapper
-public struct DefaultsValue<T> {
+public struct DefaultsValue<T: Codable>: DynamicProperty {
 	private let key: String
-	private let defaultValue: T?
+	@State private var current: T
 	
-	public init(key: String, defaultValue: T? = nil, in defaults: UserDefaults = .standard) {
+	public init(key: String, defaultValue: T, in defaults: UserDefaults = .standard) {
 		self.key = key
-		self.defaultValue = defaultValue
 		self.defaults = defaults
+		_current = State(initialValue: defaults.getCodable(for: key) ?? defaultValue)
 	}
 	
 	let defaults: UserDefaults
 	
-	public var wrappedValue: T? {
-		get { UserDefaults.standard.object(forKey: key) as? T ?? defaultValue }
-		set {
-			if let value = newValue {
-				defaults.set(value, forKey: key)
-			} else {
-				defaults.removeValue(forKey: key)
-			}
+	public var projectedValue: Binding<T> {
+		Binding<T>(get: {
+			wrappedValue
+		}) { newValue in
+			wrappedValue = newValue
+		}
+	}
+	
+	public var wrappedValue: T {
+		get { current }
+		nonmutating set {
+			current = newValue
+			defaults.setCodable(value: newValue, for: key)
+		}
+	}
+}
+
+private extension UserDefaults {
+	func setCodable<T: Codable>(value: T, for key: String) {
+		do {
+			let data = try JSONEncoder().encode(value)
+			set(data, forKey: key)
+		} catch {
+			Studio.logg(error: error, "Failed to encode DefaultsValue<\(T.self)>")
+		}
+	}
+	func getCodable<T: Codable>(for key: String) -> T? {
+		guard let data = data(forKey: key) else { return nil }
+		do {
+			return try JSONDecoder().decode(T.self, from: data)
+		} catch {
+			Studio.logg(error: error, "Failed to decode DefaultsValue<\(T.self)>")
+			return nil
 		}
 	}
 }
