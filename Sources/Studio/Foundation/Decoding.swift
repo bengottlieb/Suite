eba15969ca8f751511f5d2df46b113118a03a367
Ben Gottlieb <ben@standalone.com>
2021-12-05 06:16:19 -0600

    added some JSONDecoding additions

---
+++ Sources/Studio/Foundation/Decoding.swift
@@ -0,0 +1,51 @@
+//
+//  Decoding.swift
+//  
+//
+//  Created by Ben Gottlieb on 12/5/21.
+//
+
+import Foundation
+
+public extension KeyedDecodingContainer {
+	func decode<T: Decodable>(_ key: Key, as type: T.Type = T.self) throws -> T {
+		try self.decode(T.self, forKey: key)
+	}
+	
+	func decodeIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) throws -> T? {
+		try decodeIfPresent(T.self, forKey: key)
+	}
+}
+
+public struct SafeDecodable<Base: Decodable>: Decodable {
+	public let value: Base?
+	public let error: Error?
+	
+	public init(from decoder: Decoder) throws {
+		do {
+			let container = try decoder.singleValueContainer()
+			self.value = try container.decode(Base.self)
+			self.error = nil
+		} catch {
+			self.value = nil
+			self.error = error
+		}
+	}
+}
+
+public struct SafeResult<Kind: Decodable> {
+	public let array: [Kind]
+	public let errors: [Error]
+}
+
+public extension JSONDecoder {
+	func safelyDecodeArray<Kind: Decodable>(of element: Kind, from data: Data) -> SafeResult<Kind> {
+		do {
+			let results = try self.decode([SafeDecodable<Kind>].self, from: data)
+			
+			return SafeResult(array: results.compactMap { $0.value }, errors: results.compactMap { $0.error })
+		} catch {
+			return SafeResult<Kind>(array: [], errors: [error])
+		}
+	}
+}
