ee4557dfdf16ead5392a528715a62dbce18eee04
Ben Gottlieb <ben@standalone.com>
2022-01-19 13:03:07 -0600

    LoadingState is now equatable

--- Sources/Studio/Types/LoadingState.swift
+++ Sources/Studio/Types/LoadingState.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-public enum LoadingState<Value> { case idle, loading, failed(Error), loaded(Value) }
+public enum LoadingState<Value>: Equatable { case idle, loading, failed(Error), loaded(Value)
+	public static func ==(lhs: LoadingState, rhs: LoadingState) -> Bool {
+		switch (lhs, rhs) {
+		case (.idle, .idle): return true
+		case (.loading, .loading): return true
+		case (.failed, .failed): return true
+		default: return false
+		}
+	}
+	
+	public var isLoaded: Bool {
+		if case .loaded = self { return true }
+		return false
+	}
+	
+	public var error: Error? {
+		if case let .failed(error) = self { return error }
+		return nil
+	}
+	
+}
 
