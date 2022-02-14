f37c48cd775df69bf8b4cf3c901d1efa3a310d43
Ben Gottlieb <ben@standalone.com>
2022-02-14 17:13:11 -0600

    added IntSize

---
+++ Sources/Studio/Types/IntSize.swift
@@ -0,0 +1,36 @@
+//
+//  IntSize.swift
+//  
+//
+//  Created by Ben Gottlieb on 2/14/22.
+//
+
+import CoreGraphics
+
+public struct IntSize: Equatable {
+	public let width: Int
+	public let height: Int
+	
+	public init(_ w: Int, _ h: Int) { width = w; height = h }
+	init(screenW w: Int, _ h: Int) { self.init(min(w, h), max(w, h)) }
+}
+
+public struct IntPoint: Equatable {
+	public let x: Int
+	public let y: Int
+	
+	init(_ x: Int, _ y: Int) { self.x = x; self.y = y }
+}
+
+extension IntSize {
+	public static func ==(lhs: IntSize, rhs: CGSize) -> Bool {
+		lhs.width == Int(rhs.width) && lhs.height == Int(rhs.height)
+	}
+}
+
+extension IntPoint {
+	public static func ==(lhs: IntPoint, rhs: CGPoint) -> Bool {
+		lhs.x == Int(rhs.x) && lhs.y == Int(rhs.y)
+	}
+}
+
--- Sources/Studio/UIKit/ScreenSize.swift
+++ Sources/Studio/UIKit/ScreenSize.swift
@@ -8,50 +8,33 @@
 #if canImport(UIKit)
 import UIKit
 
-public struct ScreenSize {
-	public let width: Int
-	public let height: Int
-	
-	init(_ w: Int, _ h: Int) { width = min(w, h); height = max(w, h) }
-}
-
-extension ScreenSize: Equatable {
-	public static func ==(lhs: ScreenSize, rhs: ScreenSize) -> Bool {
-		lhs.width == rhs.width && lhs.height == rhs.height
-	}
-
-	public static func ==(lhs: ScreenSize, rhs: CGSize) -> Bool {
-		lhs.width == Int(rhs.width) && lhs.height == Int(rhs.height)
-	}
-}
-
 public extension CGSize {
-	var screenSize: ScreenSize {
-		ScreenSize.nearest(to: self)
+	var screenSize: IntSize {
+		IntSize.nearest(to: self)
 	}
 }
 
-public extension ScreenSize {
-	static let iPhone12ProMax = ScreenSize(428, 926)
-	static let iPhone12 = ScreenSize(390, 844)
-	static let iPhone11ProMax = ScreenSize(414, 896)
-	static let iPhoneX = ScreenSize(375, 812)
-	static let iPhoneSixPlus = ScreenSize(414, 736)
-	static let iPhoneSix = ScreenSize(375, 667)
-	static let iPhone5 = ScreenSize(320, 568)
-	static let iPhone = ScreenSize(320, 480)
+public extension IntSize {
+	static let iPhone12ProMax = IntSize(screenW: 428, 926)
+	static let iPhone12 = IntSize(screenW: 390, 844)
+	static let iPhone11ProMax = IntSize(screenW: 414, 896)
+	static let iPhoneX = IntSize(screenW: 375, 812)
+	static let iPhoneSixPlus = IntSize(screenW: 414, 736)
+	static let iPhoneSix = IntSize(screenW: 375, 667)
+	static let iPhone5 = IntSize(screenW: 320, 568)
+	static let iPhone = IntSize(screenW: 320, 480)
 	
-	static let iPadPro11 = ScreenSize(834, 1194)
-	static let iPadPro10_5 = ScreenSize(834, 1112)
-	static let iPadPro12_9 = ScreenSize(1024, 1366)
-	static let iPadAir_4thGen = ScreenSize(820, 1180)
-	static let iPad_7thGen = ScreenSize(810, 1080)
-	static let iPad = ScreenSize(768, 1024)
+	static let iPadPro11 = IntSize(screenW: 834, 1194)
+	static let iPadPro10_5 = IntSize(screenW: 834, 1112)
+	static let iPadPro12_9 = IntSize(screenW: 1024, 1366)
+	static let iPadAir_4thGen = IntSize(screenW: 820, 1180)
+	static let iPad_7thGen = IntSize(screenW: 810, 1080)
+	static let iPad = IntSize(screenW: 768, 1024)
 
-	static let phones: [ScreenSize] = [.iPhone, .iPhone5, .iPhoneSix, .iPhoneSixPlus, .iPhoneX, .iPhone11ProMax, .iPhone12, .iPhone12ProMax]
-	static let pads: [ScreenSize] = [.iPadPro11, iPadPro10_5, iPadPro12_9, iPadAir_4thGen, iPad_7thGen, iPad]
+	static let phones: [IntSize] = [.iPhone, .iPhone5, .iPhoneSix, .iPhoneSixPlus, .iPhoneX, .iPhone11ProMax, .iPhone12, .iPhone12ProMax]
+	static let pads: [IntSize] = [.iPadPro11, iPadPro10_5, iPadPro12_9, iPadAir_4thGen, iPad_7thGen, iPad]
 
-	static func nearest(to size: CGSize) -> ScreenSize {
+	static func nearest(to size: CGSize) -> IntSize {
 		var diff = 1000000
 		var closest = iPhone
 		
