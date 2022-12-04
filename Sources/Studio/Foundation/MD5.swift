7c3e8840d3ff3e9f70fdf52e5b08672cb815a06a
Ben Gottlieb <ben@standalone.com>
2022-12-04 07:25:44 -0600

    added autorelease wrappers around md5 calculations

--- Sources/Studio/Foundation/MD5.swift
+++ Sources/Studio/Foundation/MD5.swift
@@ -12,23 +12,29 @@ import CryptoKit
 @available(watchOS 6.0, iOS 13.0, macOS 10.15, *)
 public extension String {
 	var md5: String? {
-		guard let data = data(using: .utf8) else { return nil }
-		return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+		autoreleasepool {
+			guard let data = data(using: .utf8) else { return nil }
+			return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+		}
 	 }
 }
 
 @available(watchOS 6.0, iOS 13.0, macOS 10.15, *)
 public extension Data {
 	var md5: String? {
-		return Insecure.MD5.hash(data: self).map { String(format: "%02hhx", $0) }.joined()
+		autoreleasepool {
+			Insecure.MD5.hash(data: self).map { String(format: "%02hhx", $0) }.joined()
+		}
 	 }
 }
 
 @available(watchOS 6.0, iOS 13.0, macOS 10.15, *)
 public extension URL {
 	var md5: String? {
-		guard isFileURL, let data = try? Data(contentsOf: self) else { return nil }
-		return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+		autoreleasepool {
+			guard isFileURL, let data = try? Data(contentsOf: self) else { return nil }
+			return Insecure.MD5.hash(data: data).map { String(format: "%02hhx", $0) }.joined()
+		}
 	 }
 }
 
