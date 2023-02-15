8b859481bb89d50575626592d411bf4a7edc2155
Ben Gottlieb <ben@standalone.com>
2023-02-15 05:42:45 -0600

    added DebuggingIDView

---
+++ Sources/Suite/SwiftUI/DebuggingIDView.swift
@@ -0,0 +1,38 @@
+//
+//  DebuggingIDView.swift
+//  
+//
+//  Created by Ben Gottlieb on 2/15/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, macOS 12.0, *)
+public extension View {
+	@ViewBuilder func showDebuggingID(visible: Bool? = nil, _ id: String, alignment: Alignment = .topLeading) -> some View {
+		if visible ?? DebuggingIDView.showViewDebuggingIDs {
+			self
+				.overlay(alignment: alignment) {
+					DebuggingIDView(id: id)
+				}
+		} else {
+			self
+		}
+	}
+}
+
+public struct DebuggingIDView: View {
+	public static var showViewDebuggingIDs = false
+	
+	let id: String
+	public var body: some View {
+		Text(id)
+			.foregroundColor(.white)
+			.font(.caption)
+			.truncationMode(.middle)
+			.padding(3)
+			.backgroundColor(.red)
+			.shadow(radius: 3)
+	}
+}
+
