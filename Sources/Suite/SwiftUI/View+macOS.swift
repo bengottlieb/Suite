8f307f7ff526f443fc57755e7c25afe8e4933815
Ben Gottlieb <ben@standalone.com>
2023-03-12 21:30:42 -0500

    added View.cluseCurrentWindow

---
+++ Sources/Suite/SwiftUI/View+macOS.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Gottlieb on 3/12/23.
+//
+
+#if canImport(AppKit)
+
+import AppKit
+
+@available(OSX 10.15, *)
+public extension View {
+	func closeCurrentWindow() {
+		NSApplication.shared.keyWindow?.close()
+	}
+}
+
+#endif
