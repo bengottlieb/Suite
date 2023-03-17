fa9e7816017de458d07d2c5125b8bcc74d27efd8
Ben Gottlieb <ben@standalone.com>
2023-03-17 06:08:31 -0500

    added Color.hex

--- Sources/Suite/SwiftUI/Color.swift
+++ Sources/Suite/SwiftUI/Color.swift
@@ -55,6 +55,31 @@ public extension Color {
 		static var systemBackground: Color { Color(NSColor.windowBackgroundColor) }
 		static var tertiaryText: Color { Color(NSColor.tertiaryLabelColor) }
 	}
+
+	@available(OSX 11, *)
+		public extension Color {
+			var hex: String? {
+				let uic = NSColor(self)
+				guard let components = uic.cgColor.components, components.count >= 3 else {
+					return nil
+				}
+				let r = Float(components[0])
+				let g = Float(components[1])
+				let b = Float(components[2])
+				var a = Float(1.0)
+				
+				if components.count >= 4 {
+					a = Float(components[3])
+				}
+				
+				if a != Float(1.0) {
+					return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+				} else {
+					return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+				}
+			}
+		}
+
 #elseif canImport(UIKit) && os(iOS)
 	import UIKit
 
@@ -63,6 +88,30 @@ public extension Color {
 		static var systemBackground: Color { Color(UIColor.systemBackground) }
 		static var systemLabel: Color { Color(UIColor.label) }
 		static var tertiaryText: Color { Color(UIColor.tertiaryLabel) }
+	}
+
+	@available(iOS 14.0, tvOS 13, watchOS 6, *)
+	public extension Color {
+		var hex: String? {
+			let uic = UIColor(self)
+			guard let components = uic.cgColor.components, components.count >= 3 else {
+				return nil
+			}
+			let r = Float(components[0])
+			let g = Float(components[1])
+			let b = Float(components[2])
+			var a = Float(1.0)
+			
+			if components.count >= 4 {
+				a = Float(components[3])
+			}
+			
+			if a != Float(1.0) {
+				return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+			} else {
+				return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+			}
+		}
 	}
 #endif
 
