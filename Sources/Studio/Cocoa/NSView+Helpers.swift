a4a320ccee81ec16921c768b22575c2615350ef1
Ben Gottlieb <ben@standalone.com>
2023-04-03 20:52:55 -0500

    added NSView.fullyConstrain(to:)

--- Sources/Studio/Cocoa/NSView+Helpers.swift
+++ Sources/Studio/Cocoa/NSView+Helpers.swift
@@ -108,6 +108,14 @@ public extension NSView {
 		self.layer?.borderWidth = width
 		return self
 	}
+	
+	func fullyConstrain(to view: NSView) {
+		translatesAutoresizingMaskIntoConstraints = false
+		addConstraint(NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0))
+		addConstraint(NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 23))
+		addConstraint(NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 0))
+		addConstraint(NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0))
+	}
 }
 
 
