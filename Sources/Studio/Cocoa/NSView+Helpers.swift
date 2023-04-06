166cfdcb5f7276e146cfd1d1534a8700fbe7aaa2
Ben Gottlieb <ben@standalone.com>
2023-04-05 21:09:32 -0500

    cleaned up constraint attributes

--- Sources/Studio/Cocoa/NSView+Helpers.swift
+++ Sources/Studio/Cocoa/NSView+Helpers.swift
@@ -111,9 +111,9 @@ public extension NSView {
 	
 	func fullyConstrain(to view: NSView) {
 		translatesAutoresizingMaskIntoConstraints = false
-		addConstraint(NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0))
+		addConstraint(NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0))
 		addConstraint(NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 23))
-		addConstraint(NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 0))
+		addConstraint(NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0))
 		addConstraint(NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0))
 	}
 }
