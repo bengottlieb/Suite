2cfab3cf20382a9f0197950cffe1d8068b76099b
Ben Gottlieb <ben@standalone.com>
2023-04-10 06:29:40 -0500

    added PositionedLongPress

---
+++ Sources/Suite/SwiftUI/PositionedLongPress.swift
@@ -0,0 +1,60 @@
+//
+//  PositionedLongPressGesture.swift
+//  
+//
+//  Created by Ben Gottlieb on 4/10/23.
+//
+
+import SwiftUI
+
+
+@available(OSX 10.15, iOS 13.0, watchOS 6.0, *)
+public extension View {
+	func positionedLongPressGesture(duration: TimeInterval = 1.0, maxDistance: Double = 0, completion: @escaping (CGPoint) -> Void) -> some View {
+		self
+			.modifier(PositionedLongPressGesture(duration: duration, maxDistance: maxDistance, completion: completion))
+	}
+}
+
+@available(OSX 10.15, iOS 13.0, watchOS 6.0, *)
+struct PositionedLongPressGesture: ViewModifier {
+	@State private var location: CGPoint?
+	@State private var timer: Timer?
+	let longPressDuration: TimeInterval
+	let maxDistance: Double
+	var completion: (CGPoint) -> Void
+
+	init(duration: TimeInterval = 1.0, maxDistance: Double = 0, completion: @escaping (CGPoint) -> Void) {
+		self.longPressDuration = duration
+		self.completion = completion
+		self.maxDistance = maxDistance
+	}
+	
+	func body(content: Content) -> some View {
+		content.background (
+			Color.clear
+				.contentShape(Rectangle())
+				.onTapGesture { }
+				.gesture(
+					DragGesture(minimumDistance: maxDistance, coordinateSpace: .local)
+						.onChanged { info in
+							if location == nil {
+								location = info.location
+								timer = Timer.scheduledTimer(withTimeInterval: longPressDuration, repeats: false) { _ in
+									if let location { completion(location) }
+									timer = nil
+								}
+							}
+							
+							if info.translation.largestDimension > 10 { timer?.invalidate() }
+						}
+						.onEnded { _ in
+							timer?.invalidate()
+							timer = nil
+							location = nil
+						}
+
+				)
+			)
+	}
+}
