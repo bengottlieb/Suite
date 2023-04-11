4536c10db417fcf61d934616b819845505689ebe
Ben Gottlieb <ben@standalone.com>
2023-04-11 06:47:48 -0500

    added DateTag

---
+++ Sources/Studio/Foundation/DateTag.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Gottlieb on 4/11/23.
+//
+
+import Foundation
+
+public extension Date {
+	enum DateTagStyle: String {
+		case compact = "%d-%d-%d.%d.%d"
+		case seconds = "%d-%d-%d.%d.%d.%d"
+
+		case compactColon = "%d-%d-%d.%d:%d"
+		case secondsColon = "%d-%d-%d.%d:%d:%d"
+
+	}
+	
+	func dateTag(_ style: DateTagStyle = .compact) -> String {
+		String(format: style.rawValue, year, month.rawValue, dayOfMonth, hour, minute, second)
+	}
+}
+
+public extension String {
+	static var dateTag: String { Date().dateTag() }
+}
