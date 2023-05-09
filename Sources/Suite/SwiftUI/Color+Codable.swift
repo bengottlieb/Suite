f484c04e455fb2495c3c8ed6acd480e9afe8eb9a
Ben Gottlieb <ben@standalone.com>
2023-05-09 07:24:57 -0500

    made SwiftUI.Color codable

---
+++ Sources/Suite/SwiftUI/Color+Codable.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ben Gottlieb on 5/9/23.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, tvOS 13, watchOS 6, *)
+extension Color: Codable {
+	enum ColorDecodeError: Error { case unableToExtractColor }
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		
+		let hex = try container.decode(String.self)
+		if let color = Color(hex: hex) {
+			self = color
+		} else {
+			throw ColorDecodeError.unableToExtractColor
+		}
+	}
+	
+	public func encode(to encoder: Encoder) throws {
+		var container = encoder.singleValueContainer()
+		
+		if let hex {
+			try container.encode(hex)
+		}
+	}
+}
