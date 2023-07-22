8e7a6a29e776c5ed327b47f1c9e8a2b3148e3a4c
Ben Gottlieb <ben@standalone.com>
2023-07-22 12:06:29 -0500

    added spinnerScale to AsyncButton

--- Sources/Suite/SwiftUI/AsyncButton.swift
+++ Sources/Suite/SwiftUI/AsyncButton.swift
@@ -23,19 +23,22 @@ public struct AsyncButton<Label: View>: View {
 	
 	@State private var isPerformingAction = false
 	var role: Any?
+	var spinnerScale: Double
 	
-	public init(action: @escaping () async throws -> Void, spinnerColor: Color? = nil, @ViewBuilder label: @escaping () -> Label) {
+	public init(action: @escaping () async throws -> Void, spinnerColor: Color? = nil, spinnerScale: Double = 1.0, @ViewBuilder label: @escaping () -> Label) {
 		self.action = action
 		self.label = label
 		self.spinnerColor = spinnerColor ?? .white
+		self.spinnerScale = spinnerScale
 	}
 	
 	@available(macOS 12.0, iOS 15.0, watchOS 8.0, *)
-	public init(role: ButtonRole?, action: @escaping () async throws -> Void, spinnerColor: Color? = nil, @ViewBuilder label: @escaping () -> Label) {
+	public init(role: ButtonRole?, action: @escaping () async throws -> Void, spinnerColor: Color? = nil, spinnerScale: Double = 1.0, @ViewBuilder label: @escaping () -> Label) {
 		self.action = action
 		self.label = label
 		self.role = role
 		self.spinnerColor = spinnerColor ?? .white
+		self.spinnerScale = spinnerScale
 	}
 	
 	public var body: some View {
@@ -63,20 +66,27 @@ public struct AsyncButton<Label: View>: View {
 	}
 	
 	var buttonLabel: some View {
-		ZStack {
+		VStack {
 			if isPerformingAction {
-				label().opacity(0)
-				if #available(OSX 13, iOS 16, watchOS 9, *) {
-					ProgressView()
-						.tint(spinnerColor)
-				} else if #available(OSX 11, iOS 14.0, watchOS 7, *) {
-					ProgressView()
-						.colorInvert()
-				} else {
-					label().opacity(0.2)
-				}
+				label()
+					.opacity(0.2)
 			} else {
 				label()
+			}
+		}
+		.overlay(spinner)
+	}
+	
+	@ViewBuilder var spinner: some View {
+		if isPerformingAction {
+			if #available(OSX 13, iOS 16, watchOS 9, *) {
+				ProgressView()
+					.scaleEffect(spinnerScale)
+					.tint(spinnerColor)
+			} else if #available(OSX 11, iOS 14.0, watchOS 7, *) {
+				ProgressView()
+					.scaleEffect(spinnerScale)
+					.colorInvert()
 			}
 		}
 	}
@@ -84,17 +94,19 @@ public struct AsyncButton<Label: View>: View {
 
 @available(OSX 10.15, iOS 13.0, tvOS 13, watchOS 8, *)
 extension AsyncButton where Label == Text {
-	public init(_ title: String, action: @escaping () async throws -> Void) {
+	public init(_ title: String, spinnerScale: Double = 1.0, action: @escaping () async throws -> Void) {
 		self.action = action
+		self.spinnerScale = spinnerScale
 		self.label = { Text(title) }
 	}
 }
 
 @available(macOS 12, iOS 15.0, tvOS 13, watchOS 8, *)
 extension AsyncButton where Label == Text {
-	public init(_ title: String, role: ButtonRole, action: @escaping () async throws -> Void) {
+	public init(_ title: String, role: ButtonRole, spinnerScale: Double = 1.0, action: @escaping () async throws -> Void) {
 		self.action = action
 		self.role = role
+		self.spinnerScale = spinnerScale
 		self.label = { Text(title) }
 	}
 }
