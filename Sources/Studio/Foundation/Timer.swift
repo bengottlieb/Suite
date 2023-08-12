c5ef3cb5211aa1691a3dfa146c666174e5969981
Ben Gottlieb <ben@standalone.com>
2023-08-12 06:27:25 -0500

    actually added the code

--- Sources/Studio/Foundation/Timer.swift
+++ Sources/Studio/Foundation/Timer.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  Timer.swift
 //  
 //
 //  Created by Ben Gottlieb on 8/12/23.
 //
 
+#if canImport(Combine)
+import Combine
 import Foundation
+
+public typealias AutoconnectedTimer = Publishers.Autoconnect<Timer.TimerPublisher>
+
+#endif
