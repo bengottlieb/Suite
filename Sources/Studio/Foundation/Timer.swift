980d104f750c7408ae2c402eb28c43de6f78c86a
Ben Gottlieb <ben@standalone.com>
2023-09-09 05:19:15 -0500

    added Timer.nonPausingTimer

--- Sources/Studio/Foundation/Timer.swift
+++ Sources/Studio/Foundation/Timer.swift
@@ -11,4 +11,14 @@ import Foundation
 
 public typealias AutoconnectedTimer = Publishers.Autoconnect<Timer.TimerPublisher>
 
+public extension Timer {
+	static func nonPausingTimer(withTimeInterval interval: TimeInterval, block: @escaping @Sendable (Timer) -> Void) -> Timer {
+		let timer = Timer(timeInterval: interval, repeats: true, block: block)
+		
+		RunLoop.main.add(timer, forMode: .common)
+		
+		return timer
+	}
+}
+
 #endif
