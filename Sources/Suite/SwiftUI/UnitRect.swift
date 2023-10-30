e2dcbbec448b64be0091ae0100b8ac411dafc826
Ben Gottlieb <ben@standalone.com>
2023-10-30 05:44:13 -0500

    more UnitRect methods

--- Sources/Suite/SwiftUI/UnitRect.swift
+++ Sources/Suite/SwiftUI/UnitRect.swift
@@ -26,9 +26,37 @@ public struct UnitRect: Hashable, Sendable, Equatable, CustomStringConvertible {
 	public var origin: UnitPoint
 	public var size: UnitSize
 	
+	public var x: CGFloat { origin.x }
+	public var y: CGFloat { origin.y }
+	public var width: CGFloat { size.width }
+	public var height: CGFloat { size.height }
+	public var right: CGFloat { min(1.0, x + width) }
+	public var bottom: CGFloat { min(1.0, y + height) }
+	public var midX: CGFloat { size.width / 2 + origin.x }
+	public var midY: CGFloat { size.height / 2 + origin.y }
+	public var midPoint: UnitPoint { .init(x: midX, y: midY) }
+
 	public init(origin: UnitPoint = .zero, size: UnitSize) {
 		self.origin = origin
 		self.size = size
+	}
+	
+	public init(origin: UnitPoint = .zero, bottomRight: UnitPoint = .bottomLeading) {
+		self.origin = origin
+		self.size = .init(width: bottomRight.x - origin.x, height: bottomRight.y - origin.y)
+	}
+	
+	public func contains(_ other: UnitRect) -> Bool {
+		x <= other.x && y <= other.y && bottom >= other.bottom && right >= other.right
+	}
+	
+	public func overlap(with other: UnitRect) -> UnitRect? {
+		if right < other.x || x > other.right || bottom < other.y || y > other.bottom { return nil }
+		
+		let origin = UnitPoint(x: max(x, other.x), y: max(y, other.y))
+		let bottomRight = UnitPoint(x: min(right, other.right), y: max(bottom, other.bottom))
+		
+		return UnitRect(origin: origin, bottomRight: bottomRight)
 	}
 	
 	public static var full = UnitRect(origin: .zero, size: .full)
