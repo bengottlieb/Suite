89e4b75026f733b2d3980b74da1b95501c7abc6a
Ben Gottlieb <ben@standalone.com>
2023-11-20 20:51:18 -0600

    added option to ignore cached image for urlForPrintedPage

--- Sources/Suite/SwiftUI/View+Printing.swift
+++ Sources/Suite/SwiftUI/View+Printing.swift
@@ -20,12 +20,13 @@ public extension View {
 
 public extension View {
 	@available(iOS 16.0, macOS 13.0, *)
-	@MainActor func urlForPrintedPage(named :String) -> URL? {
+    @MainActor func urlForPrintedPage(named: String, ignoreCache: Bool = false) -> URL? {
 		let url = URL.cache(named: named)
-		if FileManager.default.fileExists(at: url) { return url }
+		if !ignoreCache, FileManager.default.fileExists(at: url) { return url }
 		guard let image = imageForPrinting() else { return nil }
 		
-		guard let data = image.pngData() else { return nil }
+        try? FileManager.default.removeItem(at: url)
+        guard let data = image.pngData() else { return nil }
 		
 		do {
 			try data.write(to: url)
