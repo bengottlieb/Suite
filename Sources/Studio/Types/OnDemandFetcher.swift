f25a0a0d88e057dba003fad422e02d3c917dd87f
Ben Gottlieb <ben@standalone.com>
2023-12-23 20:45:24 -0600

    added OnDemandFetcher

---
+++ Sources/Studio/Types/OnDemandFetcher.swift
@@ -0,0 +1,37 @@
+//
+//  OnDemandFetcher.swift
+//
+//
+//  Created by Ben Gottlieb on 12/23/23.
+//
+
+import Foundation
+
+public struct OnDemandFetcher {
+	struct StoredDictionary: Codable {
+		let version: Int
+		let dictionary: [String: String]
+	}
+	
+	public static func fetchDictionary(key: String, version: Int = 1) async throws -> [String: String] {
+		let keychainKey = "ondemand_\(key)"
+		if let keychainData = Keychain.instance.data(forKey: keychainKey), let cached = try? JSONDecoder().decode(StoredDictionary.self, from: keychainData), cached.version == version {
+			return cached.dictionary
+		}
+		
+		let request = NSBundleResourceRequest(tags: [key], bundle: .main)
+		try await request.beginAccessingResources()
+		
+		let url = Bundle.main.url(forResource: "Keys", withExtension: "json")!
+		let data = try Data(contentsOf: url)
+		let json = try JSONDecoder().decode([String: String].self, from: data)
+		let cache = StoredDictionary(version: version, dictionary: json)
+		
+		if let cacheData = try? JSONEncoder().encode(cache) {
+			Keychain.instance.set(cacheData, forKey: keychainKey)
+		}
+		
+		request.endAccessingResources()
+		return json
+	}
+}
